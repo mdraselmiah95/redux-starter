4e2f4a021b9adfdc40e30865b8818916ccbd4f77
rasel <russelroy15@gmail.com>
2022-01-20 23:26:57 +0600

    code addef

--- webpack.config.js
+++ webpack.config.js
@@ -7,7 +7,7 @@ module.exports = {
   },
   plugins: [
     new HtmlWebpackPlugin({
-      title: "webpack Boilerplate",
+      title: "webpack",
       template: path.resolve(__dirname, "./src/template.html"), // template file
       filename: "index.html", // output file
     }),
