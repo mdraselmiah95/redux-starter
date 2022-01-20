927f67a56d73e7f60dc353348dc57f70bba299ff
rasel <russelroy15@gmail.com>
2022-01-21 01:40:14 +0600

    WIP on Redux starter Project

--- src/actions.js
+++ src/actions.js
@@ -3,12 +3,13 @@ import * as actions from "./actionTypes";
 export const bugAdded = (description) => ({
   type: actions.BUG_ADDED,
   payload: {
-    description: "Bug1",
+    description,
   },
 });
 
 export const bugResolved = (id) => ({
   type: actions.BUG_RESOLVED,
+  payload: { id },
 });
 
 // export function bugAdded(description) {
