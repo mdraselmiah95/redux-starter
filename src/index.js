33b9003c848cc0a21e6b3a5882a2298dbd141001
rasel <russelroy15@gmail.com>
2022-01-21 01:00:52 +0600

    WIP on Redux starter Project

--- src/index.js
+++ src/index.js
@@ -3,6 +3,4 @@ import store from "./store";
 // Append heading node to the DOM
 // const app = document.querySelector("#root");
 
-const rasel = { name: "John", age: 24 };
-console.log(rasel);
 console.log(store.getState());
