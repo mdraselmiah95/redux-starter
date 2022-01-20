30dc800bfd2bf5ef8c6532af9984402370fda3c6
rasel <russelroy15@gmail.com>
2022-01-20 21:45:34 +0600

    code addef

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,5 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
-const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 
 module.exports = {
   entry: {
@@ -12,7 +11,6 @@ module.exports = {
       template: path.resolve(__dirname, "./src/template.html"), // template file
       filename: "index.html", // output file
     }),
-    new CleanWebpackPlugin(),
   ],
   mode: "development",
 };
