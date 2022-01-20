dac23c0674deef85899100d611bb81e468143912
rasel <russelroy15@gmail.com>
2022-01-21 01:03:26 +0600

    WIP on Redux starter Project

--- src/index.js
+++ src/index.js
@@ -3,4 +3,18 @@ import store from "./store";
 // Append heading node to the DOM
 // const app = document.querySelector("#root");
 
+store.dispatch({
+  type: "bugAdded",
+  payload: {
+    description: "Bug1",
+  },
+});
+
+store.dispatch({
+  type: "bugRemoved",
+  payload: {
+    id: 1,
+  },
+});
+
 console.log(store.getState());
