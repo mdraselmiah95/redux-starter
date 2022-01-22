4def4ce8bcb9e4031d55796760cfd46f391e8cbd
rasel <russelroy15@gmail.com>
2022-01-22 19:56:50 +0600

    WIP on redux starter

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -1,9 +1,7 @@
-import { createStore } from "redux";
-import { devToolsEnhancer } from "redux-devtools-extension";
+import { configureStore } from "@reduxjs/toolkit";
 import reducer from "./bugs";
 
 // window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__();
-export default function configureStore() {
-  const store = createStore(reducer, devToolsEnhancer({ trace: true }));
-  return store;
+export default function () {
+  return configureStore({ reducer });
 }
