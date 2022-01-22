e0ebfbc64ac4e2096812d2cbf8707531e3ec5b3f
rasel <russelroy15@gmail.com>
2022-01-23 00:16:21 +0600

    WIP on redux & redux-toolkit project

--- src/store/reducer.js
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from "redux";
-import bugReducer from "./bugs";
-import projectReducer from "./projects";
-
-export default combineReducers({
-  bugs: bugReducer,
-  projects: projectReducer,
-});
