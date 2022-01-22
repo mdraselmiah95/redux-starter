4cf0668b89778ad177c34e89f7649b48ea4fdbd6
rasel <russelroy15@gmail.com>
2022-01-22 22:18:19 +0600

    WIP on redux starter

--- src/index.js
+++ src/index.js
@@ -1,34 +1,5 @@
-// import { bugAdded, bugResolved } from "./actions";
-// import store from "./store";
-// // Append heading node to the DOM
-// // const app = document.querySelector("#root");
-
-// // const unsubscribe = store.subscribe(() => {
-// //   console.log("Store change!", store.getState());
-// // });
-
-// //state = reducer(state, action);
-// //notify the subscribers
-
-// store.dispatch(bugAdded("bug 1"));
-// store.dispatch(bugResolved(1));
-
-// // unsubscribe();
-
-// // store.dispatch({
-// //   type: "bugRemoved",
-// //   payload: {
-// //     id: 1,
-// //   },
-// // });
-
-// console.log(store.getState());
-// console.log(store);
 import * as actions from "./store/bugs";
 import configureStore from "./store/configureStore";
-// store.state = 1;
-// console.log(store.state);
-// console.log(store);
 
 const store = configureStore();
 
