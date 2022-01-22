fc2bbd77114ee5c7b210a060f23200fe1595dd98
rasel <russelroy15@gmail.com>
2022-01-23 00:12:58 +0600

    WIP on redux & redux-toolkit project

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -1,5 +1,5 @@
 import { configureStore } from "@reduxjs/toolkit";
-import reducer from "./projects";
+import reducer from "./reducer";
 
 // window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__();
 export default function () {
