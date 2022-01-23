59ac955e2447d6336c28f12f099db5df750ab784
rasel <russelroy15@gmail.com>
2022-01-24 00:35:00 +0600

    WIP on Redux project

--- src/index.js
+++ src/index.js
@@ -6,5 +6,6 @@ store.dispatch((dispatch, getState) => {
   // Call an API
   // When the promise is resolved => dispatch()
   dispatch({ type: "bugsReceived", bugs: [1, 2, 4] });
+  console.log(getState());
   // If the promise is rejected => dispatch()
 });
