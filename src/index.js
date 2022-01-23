ef0839ed9d15f796d7cffa8c912ea2ee9fefd3c6
rasel <russelroy15@gmail.com>
2022-01-24 00:32:07 +0600

    WIP on Redux project

--- src/index.js
+++ src/index.js
@@ -5,5 +5,6 @@ const store = configureStore();
 store.dispatch(() => {
   // Call an API
   // When the promise is resolved => dispatch()
+  store.dispatch({ type: "bugsReceived", bugs: [1, 2, 4] });
   // If the promise is rejected => dispatch()
 });
