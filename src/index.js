e3e941c22298cda4a8b808909e42aabe8358b6b9
rasel <russelroy15@gmail.com>
2022-01-23 17:01:51 +0600

    Redux toolkit and more

--- src/index.js
+++ src/index.js
@@ -1,17 +1,22 @@
-import configureStore from "./store/configureStore";
-import { projectAdded } from "./store/projects";
+// import configureStore from "./store/configureStore";
+// import { projectAdded } from "./store/projects";
 
-const store = configureStore();
+// const store = configureStore();
 
-store.subscribe(() => {
-  console.log("Store changed!");
-});
+// store.subscribe(() => {
+//   console.log("Store changed!");
+// });
 
-store.dispatch(projectAdded({ name: "project 1" }));
+// store.dispatch(projectAdded({ name: "project 1" }));
 
-console.log(store.getState());
+// console.log(store.getState());
 
-const unresolvedBugs = store
-  .getState()
-  .entities.bugs.filter((bug) => !bug.resolved);
-console.log(unresolvedBugs);
+// const unresolvedBugs = store
+//   .getState()
+//   .entities.bugs.filter((bug) => !bug.resolved);
+// console.log(unresolvedBugs);
+
+import { applyMiddleware, createStore } from "redux";
+import reducer from "./store/reducer";
+
+const store = createStore(reducer, applyMiddleware(logger));
