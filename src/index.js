70fbf9d9531ea50d9b1b56191e08899135ed1702
rasel <russelroy15@gmail.com>
2022-01-24 00:30:06 +0600

    WIP on Redux project

--- src/index.js
+++ src/index.js
@@ -1,25 +1,4 @@
 import configureStore from "./store/configureStore";
-// import { projectAdded } from "./store/projects";
-
-// const store = configureStore();
-
-// store.subscribe(() => {
-//   console.log("Store changed!");
-// });
-
-// store.dispatch(projectAdded({ name: "project 1" }));
-
-// console.log(store.getState());
-
-// const unresolvedBugs = store
-//   .getState()
-//   .entities.bugs.filter((bug) => !bug.resolved);
-// console.log(unresolvedBugs);
-
-// import { applyMiddleware, createStore } from "redux";
-// import reducer from "./store/reducer";
-
-// const store = createStore(reducer, applyMiddleware(logger));
 
 const store = configureStore();
 
