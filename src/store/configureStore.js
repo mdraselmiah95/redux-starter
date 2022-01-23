38f2d24a866e5af457d5308d4721e56225a88c36
rasel <russelroy15@gmail.com>
2022-01-23 16:48:19 +0600

    Redux toolkit and more

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -1,7 +1,8 @@
 import { configureStore } from "@reduxjs/toolkit";
+import logger from "./middleware/logger";
 import reducer from "./reducer";
 
 // window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__();
 export default function () {
-  return configureStore({ reducer });
+  return configureStore({ reducer, middleware: [logger] });
 }
