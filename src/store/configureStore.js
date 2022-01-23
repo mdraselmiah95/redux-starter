65ff26dfa5167d32abf3003c1416df0e6bd1e517
rasel <russelroy15@gmail.com>
2022-01-24 00:44:01 +0600

    WIP on Redux project

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -1,11 +1,16 @@
 import { configureStore, getDefaultMiddleware } from "@reduxjs/toolkit";
 import logger from "./middleware/logger";
+import toast from "./middleware/toast";
 import reducer from "./reducer";
 
 // window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__();
 export default function () {
   return configureStore({
     reducer,
-    middleware: [...getDefaultMiddleware(), logger({ destination: "console" })],
+    middleware: [
+      ...getDefaultMiddleware(),
+      logger({ destination: "console" }),
+      toast,
+    ],
   });
 }
