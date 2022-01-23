0947b994cc78d987122cff0d298467dc72b5853b
rasel <russelroy15@gmail.com>
2022-01-24 00:38:59 +0600

    WIP on Redux project

--- src/index.js
+++ src/index.js
@@ -2,10 +2,7 @@ import configureStore from "./store/configureStore";
 
 const store = configureStore();
 
-store.dispatch((dispatch, getState) => {
-  // Call an API
-  // When the promise is resolved => dispatch()
-  dispatch({ type: "bugsReceived", bugs: [1, 2, 4] });
-  console.log(getState());
-  // If the promise is rejected => dispatch()
+store.dispatch({
+  type: "error",
+  payload: { massage: "An error occurred" },
 });
