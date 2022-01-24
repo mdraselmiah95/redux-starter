396df3d3adf7ea67a604196f8b65cbf0862e1766
rasel <russelroy15@gmail.com>
2022-01-25 01:03:59 +0600

    WIP on Redux project and API

--- src/index.js
+++ src/index.js
@@ -1,6 +1,15 @@
+import * as actions from "./store/api";
 import configureStore from "./store/configureStore";
 
 const store = configureStore();
+
+store.dispatch(
+  actions.apiCallBegan({
+    url: "/bugs",
+    onSuccess: "bugsReceived",
+    onError: "apiRequestFailed",
+  })
+);
 
 store.dispatch({
   type: "apiCallBegan", //api request method
