4ced4dc8c66beef5ac83459ac4bb655f0d875985
rasel <russelroy15@gmail.com>
2022-01-24 20:29:38 +0600

    WIP on Redux project and API

--- src/store/middleware/api.js
+++ src/store/middleware/api.js
@@ -1,3 +1,14 @@
+const action = {
+  type: "apiCallBegan", //api request method
+  payload: {
+    url: "/bugs",
+    method: "get",
+    data: {},
+    onSuccess: "bugsReceived",
+    onError: "apiRequestFailed",
+  },
+};
+
 const api = (store) => (next) => (action) => {};
 
 export default api;
