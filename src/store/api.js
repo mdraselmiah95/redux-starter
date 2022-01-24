0b8e14041a50799c7d9a2ea39b002329ab95f90a
rasel <russelroy15@gmail.com>
2022-01-25 01:02:13 +0600

    WIP on Redux project and API

--- src/store/api.js
+++ src/store/api.js
@@ -1,5 +1,5 @@
 import { createAction } from "@reduxjs/toolkit";
 
-export const apiCallBegan = createAction("apiCallBegan");
-export const apiCallSuccess = createAction("apiCallSuccess");
-export const apiCallFailed = createAction("apiCallFailed");
+export const apiCallBegan = createAction("api/CallBegan");
+export const apiCallSuccess = createAction("api/CallSuccess");
+export const apiCallFailed = createAction("api/CallFailed");
