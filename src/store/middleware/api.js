5478fe331068b8a1824700c9178695869a7cea50
rasel <russelroy15@gmail.com>
2022-01-25 01:01:53 +0600

    WIP on Redux project and API

--- src/store/middleware/api.js
+++ src/store/middleware/api.js
@@ -1,10 +1,11 @@
 import axios from "axios";
+import * as actions from "../api";
 
 const api =
   ({ dispatch }) =>
   (next) =>
   async (action) => {
-    if (action.type === "apiCallBegan") return next(action);
+    if (action.type !== actions.apiCallBegan.type) return next(action);
     next(action);
     const { url, method, data, onSuccess, onError } = action.payload;
     try {
